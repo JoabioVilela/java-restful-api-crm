476b6135b49ea529c0cb716a186913e51f838175
Joábio Vilela <joabio.vilela@gmail.com>
2024-12-06 20:08:10 -0300

    Adicionar verificação de criação do arquivo *jar.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
-
 #!/bin/bash
 # Build Script
 echo "Building project..."
-# mvn clean package -DskipTests
 mvn dependency:resolve
 mvn clean package -X
+
+# Verificar se o arquivo JAR foi gerado
+if [ -f ./target/app.jar ]; then
+    echo "JAR file exists!"
+else
+    echo "JAR file not found!"
+fi
+
+ls -al ./target  # Listar os arquivos na pasta target
