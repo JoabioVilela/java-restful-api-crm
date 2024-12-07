2a15066e887e4c43ba6e1228ca57d892e642a9b2
Joábio Vilela <joabio.vilela@gmail.com>
2024-12-07 14:59:14 -0300

    Configurar deploy no Jenkisfile e no deploy.sh.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Deploying to Kubernetes..."
 
-export KUBECONFIG=/home/joabio/.minikube/profiles/minikube/config
+echo "KUBECONFIG is set to: $KUBECONFIG"
+echo "MINIKUBE_HOME is set to: $MINIKUBE_HOME"
+
+echo "Contexto atual no cluster Kubernetes:"
+kubectl config current-context
+kubectl get nodes
+
 kubectl apply -f ./k8s/deployment.yaml
 kubectl set image deployment/my-crm-service my-crm-service=joabio/my-crm-service:latest
