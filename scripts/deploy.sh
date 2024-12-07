320df64e06bc75251398921be255c77e8a6c9015
Joábio Vilela <joabio.vilela@gmail.com>
2024-12-07 17:17:21 -0300

    Ajustar deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,13 @@ echo "MINIKUBE_HOME is set to: $MINIKUBE_HOME"
 # Configurar o ambiente no Jenkins antes de chamar comandos kubectl:
 sudo -u joabio -i minikube start
 echo "Contexto atual no cluster Kubernetes:"
-kubectl config current-context
-kubectl get nodes
+sudo -u joabio -i kubectl config current-context
+sudo -u joabio -i kubectl get nodes
 
 echo "Aplicando configurações do deployment..."
-kubectl apply -f ./k8s/deployment.yaml
+sudo -u joabio -i kubectl apply -f ./k8s/deployment.yaml
 
 echo "Atualizando imagem do deployment..."
-kubectl set image deployment/my-crm-service my-crm-service=joabio/my-crm-service:latest
+sudo -u joabio -i kubectl set image deployment/my-crm-service my-crm-service=joabio/my-crm-service:latest
 
 echo "Deploy concluído com sucesso!"
