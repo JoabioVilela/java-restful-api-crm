2ed4d431b5daf0a43ef9e1d16857ee12e98f36e3
Joábio Vilela <joabio.vilela@gmail.com>
2024-12-07 16:50:22 -0300

    Ajustar deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e  # Interrompe o script em caso de erro
+
 echo "Deploying to Kubernetes..."
 
 echo "KUBECONFIG is set to: $KUBECONFIG"
@@ -8,5 +10,10 @@ echo "Contexto atual no cluster Kubernetes:"
 kubectl config current-context
 kubectl get nodes
 
+echo "Aplicando configurações do deployment..."
 kubectl apply -f ./k8s/deployment.yaml
+
+echo "Atualizando imagem do deployment..."
 kubectl set image deployment/my-crm-service my-crm-service=joabio/my-crm-service:latest
+
+echo "Deploy concluído com sucesso!"
